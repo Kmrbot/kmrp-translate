0178e1f21367f792d3b7fb14025a574e71b2df90
oksy <852496245@qq.com>
2023-09-01 16:52:19 +0800

    【translate】翻译组件更新

--- translate_handler/translate_handler.py
+++ translate_handler/translate_handler.py
@@ -2,48 +2,37 @@ import re
 from emoji import is_emoji
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 from nonebot import on_message
-from nonebot.adapters import Event
-from nonebot.matcher import Matcher
 from nonebot.adapters.onebot.v11 import (
     Message,
 )
+from plugins import while_list_handle
 from nonebot.log import logger
 from ..translate_function import TranslatorType, get_translator
 from ..db.translator_db_utils import TranslatorDBUtils
 
 translate_handler = on_message(priority=5)
 
+translate_handler.handle()(while_list_handle("translate"))
 
 async def is_translate_user(
-        matcher: Matcher,
-        event: Event):
+        event: GroupMessageEvent):
+    if event.self_id == event.user_id:
+        # 如果发消息的和机器人是同一个号 就断掉
+        # 机器人是不会处理自己给自己发的消息的好像
+        logger.error("is_translate_user fail")
+        return False
     # 看当前群和说话人是不是对应的
-    if not isinstance(event, GroupMessageEvent):
-        # 不处理非群组会话
-        await matcher.finish()
-    if await TranslatorDBUtils.get_translator_data(
+    return await TranslatorDBUtils.get_translator_data(
         type=event.message_type,
         type_id=event.group_id,
         target_user_id=event.user_id,
-    ) is not None:
-        # 可以有后续的处理
-        # 如果发消息的和机器人是同一个号 就断掉
-        if event.self_id == event.user_id:
-            # 机器人是不会处理自己给自己发的消息的好像
-            logger.error("is_translate_user fail")
-            await matcher.finish()
-        else:
-            return
-    else:
-        await matcher.finish()
-
-translate_handler.handle()(is_translate_user)
+    ) is not None
 
 
 def translate_text_preprocess(text):
     """ 翻译字符预处理 """
     # 1. 过滤url
-    text = re.sub("(http|www)[^ ]* *", " ", text)   # 正则匹配去掉网页链接
+    text = re.sub("(http|www)[^ ]*com *", " ", text)  # 正则匹配去掉网页链接
     # 2. 去除emoji
     ret_str = ""
     for i in range(len(text)):
@@ -61,6 +50,9 @@ def translate_text_preprocess(text):
 async def _(
         event: GroupMessageEvent
 ):
+    if not await is_translate_user(event):
+        translate_handler.finish()
+
     # 获取当前的翻译器
     translator = get_translator(TranslatorType.TRANSLATOR_YOUDAO)
 
